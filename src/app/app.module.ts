a03278b3b94a3dd3890cb9ac58c5107ea298e7ab
AlexMuia31 <alexmuia31@gmail.com>
2020-11-15 18:24:49 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingLiftComponent } from './shopping-lift/shopping-lift.component';
+import { ShoppingEditComponent } from './shopping-lift/shopping-edit/shopping-edit.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingLiftComponent,
+    ShoppingEditComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shopping-lift/shopping-lift.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-lift',
+  templateUrl: './shopping-lift.component.html',
+  styleUrls: ['./shopping-lift.component.css']
+})
+export class ShoppingLiftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
