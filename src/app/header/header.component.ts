92615733a03311fca0f492ce4ecfd94f792f63dd
AlexMuia31 <alexmuia31@gmail.com>
2020-11-18 15:06:49 +0300

    data binding and event listening

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  
+export class HeaderComponent  {
+  @Output() featureSelected = new EventEmitter<string>(); //out put helps us to listen to the below event on app.component
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
   }
+  
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe','This is A test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'),
-    new Recipe('A Test Recipe','This is A test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872')
+    new Recipe('A Test Recipe','This is A test','https://hips.hearstapps.com/delish/assets/17/39/1506456246-delish-healthy-chicken-casserole-1.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+
   }
 
 }
