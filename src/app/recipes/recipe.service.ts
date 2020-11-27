22a5206f49d7dab33e95369872f851561124c4de
AlexMuia31 <alexmuia31@gmail.com>
2020-11-27 14:53:39 +0300

    Ingredients from shopping list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-lift/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -7,12 +9,27 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is A test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'),
-    new Recipe('A Test Recipe','This is A test','https://hips.hearstapps.com/delish/assets/17/39/1506456246-delish-healthy-chicken-casserole-1.jpg')
+    new Recipe('A Test Recipe',
+    'This is A test',
+    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872',
+    [
+      new Ingredient('Meat',1),
+      new Ingredient('French Fries', 20)
+    ]),
+    new Recipe('A Test Recipe',
+    'This is A test',
+    'https://hips.hearstapps.com/delish/assets/17/39/1506456246-delish-healthy-chicken-casserole-1.jpg',
+    [
+      new Ingredient('buns',3),
+      new Ingredient('meat', 20)
+    ])
   ];
+  constructor(private slService: ShoppingListService) { }
   getRecipes(){
     return this.recipes.slice();
   }
-
-  constructor() { }
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.slService.addIngredients(ingredients);
+  }
+  
 }
